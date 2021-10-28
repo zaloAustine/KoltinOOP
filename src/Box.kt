f6a147c08c9a46539a9d2cb3e6673336dfe4fcc9
Zalo Austine <austinezaloa@gmail.com>
2021-10-28 15:19:28 +0300

    getters and setters

--- src/Box.kt
+++ src/Box.kt
@@ -4,6 +4,23 @@ class Box {
     private val width:Int = 20
     private val height:Int = 5
 
+    val volume
+        get() = length*width*height
+
+    var boxName:String? = null
+    set(value){
+        if (value != null) {
+            if(value.length < 3){
+                println("Name cannot be less that 3 characters")
+            }else{
+                field = "Mr $value"
+            }
+        }else{
+            field = "Name cannot be Empty"
+        }
+    }
+
+
      fun fillContent(){
         println("Filled Box")
     }
@@ -12,5 +29,3 @@ class Box {
         println("Box Open")
     }
 }
-
-data class TestColor(private val test1:String)
--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,7 @@
 
     fun main(){
 
-        val b = Box()
-        b.fillContent()
-
-        val t1 = TestColor("")
+        val box = Box()
 
 
 
